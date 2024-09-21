9ad535e7c0e0d98037d3975c3e823b4801b23d62
ordercomplete <ordercomplete777@gmail.com>
2024-09-21 12:02:16 +0300

    create resume

--- scripts.js
+++ scripts.js
@@ -86,7 +86,7 @@ document.querySelectorAll(".skills_name").forEach((item) => {
     // Вебзапит для отримання опису
     try {
       const response = await fetch(
-        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+        `https://en.wikipedia.org/api/rest_v1/page/summary/${word}`
       );
       const data = await response.json();
 
